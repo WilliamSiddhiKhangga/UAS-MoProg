26726960406119c72845b32783ad320efa175a7c
Mitsuki7th <marcohuang552@gmail.com>
2024-11-13 14:04:47 +0700

    adjust welcome page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -11,8 +11,12 @@ class WelcomePage extends StatefulWidget {
   State<WelcomePage> createState() => _WelcomePageState();
 }
 
-class _WelcomePageState extends State<WelcomePage> {
+class _WelcomePageState extends State<WelcomePage>
+    with SingleTickerProviderStateMixin {
   PageController _pageController = PageController();
+  late AnimationController _animationController;
+  late Animation<Offset> _offsetAnimation;
+  int _currentIndex = 0;
 
   List images = [
     "fuji.png",
@@ -42,100 +46,147 @@ class _WelcomePageState extends State<WelcomePage> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    _offsetAnimation = Tween<Offset>(
+      begin: Offset(0, 0),
+      end: Offset(0, 0.2),
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+
+    _pageController.addListener(() {
+      setState(() {
+        _currentIndex = _pageController.page!.round();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        controller: _pageController,
-        scrollDirection: Axis.vertical,
-        itemCount: images.length,
-        itemBuilder: (context, index) {
-          return AnimatedBuilder(
-            animation: _pageController,
-            builder: (context, child) {
-              double scale = 1.0;
-              double opacity = 1.0;
-
-              if (_pageController.position.haveDimensions) {
-                double pageOffset = _pageController.page! - index;
-                scale = 1 - (pageOffset.abs() * 0.3); // Scaling effect
-                opacity = 1 - (pageOffset.abs() * 0.5); // Fade effect
-              }
-
-              return Opacity(
-                opacity: opacity.clamp(0.0, 1.0),
-                child: Transform.scale(
-                  scale: scale.clamp(0.7, 1.0),
-                  child: Container(
-                    width: double.maxFinite,
-                    height: double.maxFinite,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("img/" + images[index]),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                    child: Container(
-                      margin:
-                          const EdgeInsets.only(top: 150, left: 20, right: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            scrollDirection: Axis.vertical,
+            itemCount: images.length,
+            itemBuilder: (context, index) {
+              return Container(
+                width: double.maxFinite,
+                height: double.maxFinite,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("img/" + images[index]),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.3), BlendMode.darken),
+                  ),
+                ),
+                child: Container(
+                  margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              AppLargeText(
-                                text: content[index]["title"]!,
-                              ),
-                              AppText(
-                                text: content[index]["subtitle"]!,
-                                size: 20,
-                              ),
-                              const SizedBox(height: 20),
-                              SizedBox(
-                                width: 250,
-                                child: AppText(
-                                  text: content[index]["description"]!,
-                                  color: Colors.black54,
-                                  size: 14,
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                AppLargeText(
+                                  text: content[index]["title"]!,
+                                  color: Colors.white,
                                 ),
-                              ),
-                              const SizedBox(height: 40),
-                              ResponsiveButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => MainPage(),
-                                    ),
-                                  );
-                                },
-                              ),
-                            ],
-                          ),
-                          Column(
-                            children: List.generate(images.length, (indexDots) {
-                              return Container(
-                                margin: const EdgeInsets.only(bottom: 3),
-                                width: 8,
-                                height: index == indexDots ? 25 : 8,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: index == indexDots
-                                      ? Colors.black54
-                                      : Colors.black54.withOpacity(0.3),
+                                const SizedBox(height: 10),
+                                AppText(
+                                  text: content[index]["subtitle"]!,
+                                  size: 20,
+                                  color: Colors.white,
                                 ),
-                              );
-                            }),
+                                const SizedBox(height: 20),
+                                SizedBox(
+                                  width: 250,
+                                  child: AppText(
+                                    text: content[index]["description"]!,
+                                    color: Colors.white,
+                                    size: 14,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
+                          const SizedBox(height: 40),
+                          if (index ==
+                              images.length -
+                                  1) // Hanya tampilkan di halaman terakhir
+                            ResponsiveButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => MainPage(),
+                                  ),
+                                );
+                              },
+                            ),
                         ],
                       ),
-                    ),
+                      Column(
+                        children: List.generate(images.length, (indexDots) {
+                          return Container(
+                            margin: const EdgeInsets.only(bottom: 3),
+                            width: 8,
+                            height: _currentIndex == indexDots ? 25 : 8,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: _currentIndex == indexDots
+                                  ? Colors.black54
+                                  : Colors.black54.withOpacity(0.3),
+                            ),
+                          );
+                        }),
+                      ),
+                    ],
                   ),
                 ),
               );
             },
-          );
-        },
+          ),
+          // Panah ke bawah dengan animasi, disembunyikan di halaman terakhir
+          if (_currentIndex != images.length - 1)
+            Positioned(
+              bottom: 20,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: SlideTransition(
+                  position: _offsetAnimation,
+                  child: Icon(
+                    Icons.keyboard_arrow_down,
+                    color: Colors.white.withOpacity(0.7),
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
